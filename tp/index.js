a342618f20d86c2059e16c74f43392f5268d2555
DrAtsiSama <dareka@outlook.fr>
2023-11-06 18:06:59 +0100

    Correction naming

--- tp/index.js
+++ tp/index.js
@@ -4,9 +4,9 @@ function diceGameAnalyser(){
 
     for(let i =0; i< numberOfDice; i++){
         // simulation d'un dé entre 1 et 6
-        const result = Math.floor(Math.random() * 6) + 1;
+        const diceResult = Math.floor(Math.random() * 6) + 1;
         // ajout du résultat
-        diceResults.push(result);
+        diceResults.push(diceResult);
     }
     return sortNumberAscending(diceResults);
 };
@@ -15,7 +15,10 @@ function sortNumberAscending(list){
     return list.sort((a, b) => a - b);
 }
 
+
+
 const yamsGame = () => {
+
     return diceGameAnalyser();
 }
 
