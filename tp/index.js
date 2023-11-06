6f33b1c103a7970b643d0599fb5d5b6687db4cd6
DrAtsiSama <dareka@outlook.fr>
2023-11-06 18:28:09 +0100

    ajout du nombres de valeurs identiques

--- tp/index.js
+++ tp/index.js
@@ -1,3 +1,4 @@
+// Jet de Dés d'une partie
 function diceGameAnalyser(){
     let numberOfDice = 5;
     let diceResults = [];
@@ -11,15 +12,42 @@ function diceGameAnalyser(){
     return sortNumberAscending(diceResults);
 };
 
+// Tri croissant
 function sortNumberAscending(list){
     return list.sort((a, b) => a - b);
 }
 
+// Calcul des figures possibles
+function calculatePoints(game){
+    // nombres de valeurs identiques
+  const valueCount = {};
 
+  // Parcourez la liste et comptez les occurrences de chaque valeur
+  for (const value of game) {
+    if (valueCount[value]) {
+      valueCount[value]++;
+    } else {
+      valueCount[value] = 1;
+    }
+  }
 
+  // Comptez le nombre de valeurs identiques
+  let identicalCount = 0;
+  for (const count of Object.values(valueCount)) {
+    if (count > 1) {
+      identicalCount += count;
+    }
+  }
+  return identicalCount;
+
+}
+
+// Main
 const yamsGame = () => {
+    const game = diceGameAnalyser();
+    let score = calculatePoints(game);
 
     return diceGameAnalyser();
 }
 
-module.exports = yamsGame;
+module.exports = {yamsGame, calculatePoints};
--- tp/tests/index.test.js
+++ tp/tests/index.test.js
@@ -1,38 +1,65 @@
 const yams = require("../");
 describe("TP Yams", () => {
-    describe("Test lancé de de(s)", () => {
-        it("#Doit retourner une liste de 5 valeurs", ()=> {
-            expect(yams()).toHaveLength(5)
-        });
-        it('#Doit retourner une liste de 5 nombres', () => {
-            const result = yams();
-            result.forEach((value) => {
-              expect(typeof value).toBe('number');
-            });
-        });
-        it("#Doit retourner une liste de valeurs triées dans l'ordre croissant", () => {
-            const result = yams();
-            const isSorted = (list) => {
-                for (let i = 1; i < list.length; i++) {
-                  if (list[i] < list[i - 1]) {
-                    return false;
-                  }
-                }
-                return true;
-              };
-            expect(isSorted(result)).toBeTruthy()
-        });
-        it("#Doit retourner faux à une liste de valeurs non-triées dans l'ordre croissant", () => {
-            const result = [1,2,5,3,2];
-            const isSorted = (list) => {
-                for (let i = 1; i < list.length; i++) {
-                  if (list[i] < list[i - 1]) {
-                    return false;
-                  }
-                }
-                return true;
-              };
-            expect(isSorted(result)).toBeFalsy()
-        });   
+  describe("Test lancé de de(s)", () => {
+    it("#Doit retourner une liste de 5 valeurs", () => {
+      expect(yams.yamsGame()).toHaveLength(5)
     });
+    it('#Doit retourner une liste de 5 nombres', () => {
+      const result = yams.yamsGame();
+      result.forEach((value) => {
+        expect(typeof value).toBe('number');
+      });
+    });
+    it("#Doit retourner une liste de valeurs triées dans l'ordre croissant", () => {
+      const result = yams.yamsGame();
+      const isSorted = (list) => {
+        for (let i = 1; i < list.length; i++) {
+          if (list[i] < list[i - 1]) {
+            return false;
+          }
+        }
+        return true;
+      };
+      expect(isSorted(result)).toBeTruthy()
+    });
+    it("#Doit retourner faux à une liste de valeurs non-triées dans l'ordre croissant", () => {
+      const result = [1, 2, 5, 3, 2];
+      const isSorted = (list) => {
+        for (let i = 1; i < list.length; i++) {
+          if (list[i] < list[i - 1]) {
+            return false;
+          }
+        }
+        return true;
+      };
+      expect(isSorted(result)).toBeFalsy()
+    });
+  });
+  describe("Test du calcul de points", () => {
+    it("#Doit retourner le nombre de valeurs identiques", () => {
+      const game = yams.yamsGame();
+      const result = yams.calculatePoints(game);
+      expect(result).not.toBe(null);
+    });
+    it("#Doit retourner 0", () => {
+      const game = [1, 2, 3, 4, 5];
+      const result = yams.calculatePoints(game);
+      expect(result).toBe(0);
+    });
+    it("#Doit retourner 2", () => {
+      const game = [1, 2, 1, 4, 5];
+      const result = yams.calculatePoints(game);
+      expect(result).toBe(2);
+    });
+    it("#Doit retourner 4", () => {
+      const game = [1, 2, 1, 4, 2];
+      const result = yams.calculatePoints(game);
+      expect(result).toBe(4);
+    });
+    it("#Doit retourner 5", () => {
+      const game = [1, 2, 1, 2, 2];
+      const result = yams.calculatePoints(game);
+      expect(result).toBe(5);
+    });
+  });
 });
